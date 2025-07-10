6fcd91c9698b345b7c71d512a835e74234b044ee
Stephen Lewis <st.rayis1085@gmail.com>
2025-07-10 13:37:31 -0400

    push before work

--- Lesson-2/main.js
+++ Lesson-2/main.js
@@ -101,12 +101,20 @@ function noVowels(str) {
     let it = "";
     let vowels = "";
     console.log(str)
-    for (let s of str) {
-        s !== "a" ? it += s : s !== "e" ? it += s : s !== "i" ? it += s : s !== "o" ? it += s : s !== "u" ? it += s : vowels += s; 
-        }
-   
-                   
-        console.log(it)
+
+    function isVowel(str) {
+        switch (str) {
+        case "a":
+            return true;
+        case "e":
+            return true;
+        case "i":
+            return true;
+        case "o":
+            return true;
+        case "u":
+            return true;
+    }}
     
     // console.log("Q2: ",newStr) 
 }
