a88ec6855f6196c8125d0479488be6178b03b170
Stephen Lewis <st.rayis1085@gmail.com>
2025-08-04 11:53:51 -0400

    first algorithm

---
+++ Lesson-5/main.js
@@ -0,0 +1,32 @@
+/************
+ *ALGORITHMS*
+ ************/
+let thisStr = "this is so done with"
+let scaredFaces = stringToCaps(thisStr)
+    console.log(scaredFaces)
+
+function stringToCaps(string) {
+    let array = string.toUpperCase().split(" ")//to uppercase and put in array 
+    let lastCheck = array.length - 1;
+    let newStr = array.map((s) => {
+        let check = array.indexOf(s)
+        console.log(check, lastCheck)
+        if (check === lastCheck) {
+            
+            return s + " !"
+        }
+       return `${s} \u{1F631} `
+    })
+   
+    console.log(newStr)
+return newStr.join(" ")
+}
+/**
+ * NOTES:
+ * could have used a for.of loop but i wanted to use map again
+ * let hold = ""
+ * for(let str of nowStr){
+ * hold += str+" \u{1F563} "
+ * }
+ * return hold
+ */
